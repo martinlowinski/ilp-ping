1f881104ec5caeeb7eb53f1a53eba3f87848371a
Martin Lowinski <martin@goldtopf.org>
2019-04-18 13:21:26 +0200

    Refactoring

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const makePlugin = require('ilp-plugin')
 const crypto = require('crypto')
 const IlpPacket = require('ilp-packet')
 const ILDCP = require('ilp-protocol-ildcp')
+const Ping = require('./src/ping')
 const { Writer } = require('oer-utils')
 const log = require('ilp-logger')('ilp-ping')
 
@@ -44,60 +45,6 @@ const destination = argv.url || argv._[0]
 if (argv.destination && argv._[0]) die('cannot specify --destination and positional <destination>')
 if (!destination) die('must specify a destination with positional <destination> or --destination')
 
-class Ping {
-  constructor (deps) {
-    this.conditionMap = new Map()
-    this.plugin = makePlugin()
-  }
-
-  async init () {
-    await this.plugin.connect()
-
-    this.plugin.registerDataHandler(data => {
-      const { executionCondition } = IlpPacket.deserializeIlpPrepare(data)
-
-      const fulfillment = this.conditionMap.get(executionCondition.toString('base64'))
-      if (fulfillment) {
-        this.conditionMap.delete(executionCondition.toString('base64'))
-        return IlpPacket.serializeIlpFulfill({
-          fulfillment: fulfillment,
-          data: Buffer.alloc(0)
-        })
-      } else {
-        throw new Error('unexpected packet.')
-      }
-    })
-  }
-
-  async ping (destination) {
-    const fulfillment = crypto.randomBytes(32)
-    const condition = crypto.createHash('sha256').update(fulfillment).digest()
-    const { clientAddress } = await ILDCP.fetch(this.plugin.sendData.bind(this.plugin))
-
-    this.conditionMap.set(condition.toString('base64'), fulfillment)
-
-    const writer = new Writer()
-
-    writer.write(Buffer.from('ECHOECHOECHOECHO', 'ascii'))
-    writer.writeUInt8(0)
-    writer.writeVarOctetString(Buffer.from(clientAddress, 'ascii'))
-
-    const start = process.hrtime();
-    const result = await this.plugin.sendData(IlpPacket.serializeIlpPrepare({
-      destination,
-      amount: argv.amount.toString(),
-      executionCondition: condition,
-      expiresAt: new Date(Date.now() + argv.expiration),
-      data: writer.getBuffer()
-    }))
-    const diff = process.hrtime(start);
-    const latency = diff[0] * 1000 + diff[1] / 1000000
-    const parsedPacket = IlpPacket.deserializeIlpPacket(result)
-
-    return { parsedPacket, latency }
-  }
-}
-
 async function run () {
   console.log(`ILP-PING ${destination}`);
   log.debug(`Using amount=${argv.amount} and expiration=${argv.expiration}ms`)
@@ -111,7 +58,7 @@ async function run () {
 
   var i;
   for (i = 0; i < argv.count; i++) {
-    const {parsedPacket, latency} = await p.ping(destination);
+    const {parsedPacket, latency} = await p.ping(destination, argv.amount, argv.expiration);
 
     if (parsedPacket.type === IlpPacket.Type.TYPE_ILP_FULFILL) {
       console.log(`ILP_FULFILL from ${destination}: time=${latency}ms`)
---
+++ src/ping.js
@@ -0,0 +1,61 @@
+const makePlugin = require('ilp-plugin')
+const crypto = require('crypto')
+const IlpPacket = require('ilp-packet')
+const ILDCP = require('ilp-protocol-ildcp')
+const { Writer } = require('oer-utils')
+
+class Ping {
+    constructor () {
+        this.conditionMap = new Map()
+        this.plugin = makePlugin()
+    }
+
+    async init () {
+        await this.plugin.connect()
+
+        this.plugin.registerDataHandler(data => {
+            const { executionCondition } = IlpPacket.deserializeIlpPrepare(data)
+
+            const fulfillment = this.conditionMap.get(executionCondition.toString('base64'))
+            if (fulfillment) {
+                this.conditionMap.delete(executionCondition.toString('base64'))
+                return IlpPacket.serializeIlpFulfill({
+                    fulfillment: fulfillment,
+                    data: Buffer.alloc(0)
+                })
+            } else {
+                throw new Error('unexpected packet.')
+            }
+        })
+    }
+
+    async ping (destination, amount, expiration) {
+        const fulfillment = crypto.randomBytes(32)
+        const condition = crypto.createHash('sha256').update(fulfillment).digest()
+        const { clientAddress } = await ILDCP.fetch(this.plugin.sendData.bind(this.plugin))
+
+        this.conditionMap.set(condition.toString('base64'), fulfillment)
+
+        const writer = new Writer()
+
+        writer.write(Buffer.from('ECHOECHOECHOECHO', 'ascii'))
+        writer.writeUInt8(0)
+        writer.writeVarOctetString(Buffer.from(clientAddress, 'ascii'))
+
+        const start = process.hrtime();
+        const result = await this.plugin.sendData(IlpPacket.serializeIlpPrepare({
+            destination,
+            amount: amount.toString(),
+            executionCondition: condition,
+            expiresAt: new Date(Date.now() + expiration),
+            data: writer.getBuffer()
+        }))
+        const diff = process.hrtime(start);
+        const latency = diff[0] * 1000 + diff[1] / 1000000
+        const parsedPacket = IlpPacket.deserializeIlpPacket(result)
+
+        return { parsedPacket, latency }
+    }
+}
+
+module.exports = Ping
